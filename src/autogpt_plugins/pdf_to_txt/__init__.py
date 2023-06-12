796abe77614dfde1f9cf0837581dbc4d227566b1
paruparu <beg5db@gmail.com>
2023-06-12 17:04:38 +0900

    deleting files of other plugin for preventing plugin conflict

--- src/autogpt_plugins/pdf_to_txt/__init__.py
+++ src/autogpt_plugins/pdf_to_txt/__init__.py
@@ -1,11 +1,6 @@
-"""Telegram controller bot integration using python-telegram-bot."""
-import os
-import re
+"""This is the PDF to Text plugin for AutoGPT."""
 from typing import Any, Dict, List, Optional, Tuple, TypedDict, TypeVar
-
 from auto_gpt_plugin_template import AutoGPTPluginTemplate
-
-from .telegram_chat import TelegramUtils
 
 PromptGenerator = TypeVar("PromptGenerator")
 
@@ -15,33 +10,20 @@ class Message(TypedDict):
     content: str
 
 
-def remove_color_codes(s: str) -> str:
-    ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
-    return ansi_escape.sub("", s)
-
-
-class AutoGPTTelegram(AutoGPTPluginTemplate):
+class AutoGPTPDFToText(AutoGPTPluginTemplate):
     """
-    Telegram controller bot integration using python-telegram-bot.
+    PDF-to-Text integrations using PyMuPDF
     """
 
     def __init__(self):
         super().__init__()
-        self._name = "Auto-GPT-Telegram"
-        self._version = "0.2.0"
-        self._description = (
-            "This integrates a Telegram chat bot with your autogpt instance."
-        )
-        self.telegram_api_key = os.getenv("TELEGRAM_API_KEY", None)
-        self.telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID", None)
-        self.telegram_utils = TelegramUtils(
-            chat_id=self.telegram_chat_id, api_key=self.telegram_api_key
-        )
+        self._name = "autogpt-pdf2txt"
+        self._version = "0.1.0"
+        self._description = "PDF-to-Text integrations using PyMuPDF"
 
     def can_handle_on_response(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_response method.
-
         Returns:
             bool: True if the plugin can handle the on_response method."""
         return False
@@ -53,36 +35,21 @@ class AutoGPTTelegram(AutoGPTPluginTemplate):
     def can_handle_post_prompt(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_prompt method.
-
         Returns:
             bool: True if the plugin can handle the post_prompt method."""
-        return False
-
-    def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
-        """This method is called just after the generate_prompt is called,
-            but actually before the prompt is generated.
-
-        Args:
-            prompt (PromptGenerator): The prompt generator.
-
-        Returns:
-            PromptGenerator: The prompt generator.
-        """
-        pass
+        return True
 
     def can_handle_on_planning(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_planning method.
-
         Returns:
             bool: True if the plugin can handle the on_planning method."""
         return False
 
     def on_planning(
-        self, prompt: PromptGenerator, messages: List[Message]
+        self, prompt: PromptGenerator, messages: List[str]
     ) -> Optional[str]:
-        """This method is called before the planning chat completion is done.
-
+        """This method is called before the planning chat completeion is done.
         Args:
             prompt (PromptGenerator): The prompt generator.
             messages (List[str]): The list of messages.
@@ -92,17 +59,14 @@ class AutoGPTTelegram(AutoGPTPluginTemplate):
     def can_handle_post_planning(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_planning method.
-
         Returns:
             bool: True if the plugin can handle the post_planning method."""
         return False
 
     def post_planning(self, response: str) -> str:
-        """This method is called after the planning chat completion is done.
-
+        """This method is called after the planning chat completeion is done.
         Args:
             response (str): The response.
-
         Returns:
             str: The resulting response.
         """
@@ -111,36 +75,30 @@ class AutoGPTTelegram(AutoGPTPluginTemplate):
     def can_handle_pre_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the pre_instruction method.
-
         Returns:
             bool: True if the plugin can handle the pre_instruction method."""
         return False
 
-    def pre_instruction(self, messages: List[Message]) -> List[Message]:
+    def pre_instruction(self, messages: List[str]) -> List[str]:
         """This method is called before the instruction chat is done.
-
         Args:
-            messages (List[Message]): The list of context messages.
-
+            messages (List[str]): The list of context messages.
         Returns:
-            List[Message]: The resulting list of messages.
+            List[str]: The resulting list of messages.
         """
         pass
 
     def can_handle_on_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_instruction method.
-
         Returns:
             bool: True if the plugin can handle the on_instruction method."""
         return False
 
-    def on_instruction(self, messages: List[Message]) -> Optional[str]:
+    def on_instruction(self, messages: List[str]) -> Optional[str]:
         """This method is called when the instruction chat is done.
-
         Args:
-            messages (List[Message]): The list of context messages.
-
+            messages (List[str]): The list of context messages.
         Returns:
             Optional[str]: The resulting message.
         """
@@ -149,17 +107,14 @@ class AutoGPTTelegram(AutoGPTPluginTemplate):
     def can_handle_post_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_instruction method.
-
         Returns:
             bool: True if the plugin can handle the post_instruction method."""
         return False
 
     def post_instruction(self, response: str) -> str:
         """This method is called after the instruction chat is done.
-
         Args:
             response (str): The response.
-
         Returns:
             str: The resulting response.
         """
@@ -168,7 +123,6 @@ class AutoGPTTelegram(AutoGPTPluginTemplate):
     def can_handle_pre_command(self) -> bool:
         """This method is called to check that the plugin can
         handle the pre_command method.
-
         Returns:
             bool: True if the plugin can handle the pre_command method."""
         return False
@@ -177,11 +131,9 @@ class AutoGPTTelegram(AutoGPTPluginTemplate):
         self, command_name: str, arguments: Dict[str, Any]
     ) -> Tuple[str, Dict[str, Any]]:
         """This method is called before the command is executed.
-
         Args:
             command_name (str): The command name.
             arguments (Dict[str, Any]): The arguments.
-
         Returns:
             Tuple[str, Dict[str, Any]]: The command name and the arguments.
         """
@@ -190,82 +142,77 @@ class AutoGPTTelegram(AutoGPTPluginTemplate):
     def can_handle_post_command(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_command method.
-
         Returns:
             bool: True if the plugin can handle the post_command method."""
         return False
 
     def post_command(self, command_name: str, response: str) -> str:
         """This method is called after the command is executed.
-
         Args:
             command_name (str): The command name.
             response (str): The response.
-
         Returns:
             str: The resulting response.
         """
         pass
 
     def can_handle_chat_completion(
-        self, messages: Dict[Any, Any], model: str, temperature: float, max_tokens: int
+        self,
+        messages: list[Dict[Any, Any]],
+        model: str,
+        temperature: float,
+        max_tokens: int,
     ) -> bool:
         """This method is called to check that the plugin can
-          handle the chat_completion method.
-
+        handle the chat_completion method.
         Args:
-            messages (List[Message]): The messages.
+            messages (Dict[Any, Any]): The messages.
             model (str): The model name.
             temperature (float): The temperature.
             max_tokens (int): The max tokens.
-
-          Returns:
-              bool: True if the plugin can handle the chat_completion method."""
+        Returns:
+            bool: True if the plugin can handle the chat_completion method."""
         return False
 
     def handle_chat_completion(
-        self, messages: List[Message], model: str, temperature: float, max_tokens: int
+        self,
+        messages: list[Dict[Any, Any]],
+        model: str,
+        temperature: float,
+        max_tokens: int,
     ) -> str:
         """This method is called when the chat completion is done.
-
         Args:
-            messages (List[Message]): The messages.
+            messages (Dict[Any, Any]): The messages.
             model (str): The model name.
             temperature (float): The temperature.
             max_tokens (int): The max tokens.
-
         Returns:
             str: The resulting response.
         """
-        pass
+        return None
 
-    def can_handle_user_input(self, user_input: str) -> bool:
-        """This method is called to check that the plugin can
-        handle the user_input method.
-
+    def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
+        """This method is called just after the generate_prompt is called,
+            but actually before the prompt is generated.
         Args:
-            user_input (str): The user input.
-
+            prompt (PromptGenerator): The prompt generator.
         Returns:
-            bool: True if the plugin can handle the user_input method."""
-        return True
+            PromptGenerator: The prompt generator.
+        """
 
-    def user_input(self, user_input: str) -> str:
-        user_input = remove_color_codes(user_input)
-        # if the user_input is too long, shorten it
+        from .pdf_to_txt import (
+            pdf2txt,
+        )
 
-        return self.telegram_utils.ask_user(prompt=user_input)
+        prompt.add_command(
+            "pdf_to_txt",
+            "PDF to Text",
+            {
+                "pdf_file_name": "<pdf_file_name>",
+                "txt_file_name": "<txt_file_name>"
+            },
+            pdf2txt
+        )
 
-    def can_handle_report(self) -> bool:
-        """This method is called to check that the plugin can
-        handle the report method.
-
-        Returns:
-            bool: True if the plugin can handle the report method."""
-        return True
-
-    def report(self, message: str) -> None:
-        message = remove_color_codes(message)
-        # if the message is too long, shorten it
-
-        self.telegram_utils.send_message(message=message)
+        return prompt
